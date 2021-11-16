d59341854ad155c40a53cc9f1d5000350d65e42e
Gorata-Code <94143890+Gorata-Code@users.noreply.github.com>
2021-11-16 12:21:49 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,38 @@
 from catalog.catalog import Catalog
 
 
-search_name = input('Enter Artist / Channel Name: ').title()
+def tuber_droid(search_name):
+    try:
 
-try:
+        with Catalog() as android:
+            android.launch_the_tube(search_name)
+            android.channel_manoeuvre(f'{search_name}')
+            android.refresh()
+            android.load_entire_page()
+            android.find_the_uploads(search_name)
 
-    with Catalog() as android:
-        android.launch_the_tube(search_name)
-        android.channel_manoeuvre(f'{search_name}')
-        android.refresh()
-        android.load_entire_page()
-        android.find_the_uploads(search_name)
+    except Exception as e:
+        if 'in PATH' in str(e):
+            print(''''
+                You need to download & add Google Chrome's Webdriver to the PATH variable\n
+                Copy & paste the following line and then edit accordingly:
+                    set PATH=%PATH%;C:path-location-of-the-Webdriver
+                ''')
 
-except Exception as e:
-    if 'in PATH' in str(e):
-        print(''''
-            You need to download & add Google Chrome's Webriver to the PATH variable\n
-            Copy & paste the following line and then edit accordingly:
-                set PATH=%PATH%;C:path-location-of-the-Webdriver
-            ''')
+        elif 'ERR_INTERNET_' in str(e):
+            print(''''
+                Please make sure you are connected to the internet and Try again.
+                Cheers!
+                ''')
 
-    else:
-        raise
+        else:
 
+            raise
+
+def main():
+    search_name = input('Enter Artist / Channel Name: ').title()
+    tuber_droid(search_name)
+
+
+if __name__ == '__main__':
+    main()
