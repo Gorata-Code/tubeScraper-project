b4fa7237f36d5e1fe0a7e6bf73639828bd2c0dbf
Gorata-Code <94143890+Gorata-Code@users.noreply.github.com>
2022-07-24 22:56:13 +0200

    Update run.py
    - Added browser version detection
    - Improved exceptions handling

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import sys
 from catalog.catalog import Catalog
+from win32com.client import Dispatch
 from selenium.common.exceptions import WebDriverException
 
 
 def script_summary() -> None:
     print('''
-        \t\tDUMELANG means GREETINGS! ~ G-CODE\n
-        \t"THE-TUBE-SCRAPER" Version 1.1.0\n
+               ***----------------------------------------------------------------------------------------***
+         \t***------------------------ DUMELANG means GREETINGS! ~ G-CODE -----------------------***
+                     \t***------------------------------------------------------------------------***\n
+              
+        \t"THE-TUBE-SCRAPER" Version 1.2.0\n
+        
         This script will help you collect video metadata from any YouTube channel.\n
-        No need for installation. Just double-click this "the_tube_scraper.exe" to run it.\n
+        No need for installation. Just double-click this "The Tube Scraper.exe" to run it.\n
         The script will collect the channel data and video details and save them in a csv\n
         file, you can then open this csv file with any spreadsheet program e.g. (Microsoft Excel)\n
         and view the information you have gathered. Please make sure you keep this window\n
@@ -27,23 +32,37 @@ def tuber_droid(search_name: str) -> None:
 
     except Exception as exp:
 
-        if 'in PATH' in str(exp):
+        if 'executable needs to be in PATH' in str(exp):
+            print('''
+                       Please make sure you have not deleted or moved or renamed the "chromedriver.exe"
 
-            print(''''
+                       file that is next to the "The Tube Scraper.exe". The programme needs it
 
-                            Please make sure you have not deleted or moved or renamed the "chromedriver.exe"
+                       to work.
 
-                            file that is next to the "the_tube_scraper.exe". The programme needs it to work.
-
-                            If you still get this error, then you need to download the chromedriver for your 
-
-                            version of Chrome. There are many videos on YouTube about how to get that set up.
-
-                            Once you are done downloading it, you unzip it and then replace the current one.
-
-                            ''')
+                       ''')
 
             input('\nPress Enter To Exit.\n')
+            sys.exit(1)
+
+        elif 'version of ChromeDriver only supports Chrome version' in str(exp):
+            message: str = str(exp).split('\n')[0].split(':')[-1]
+            print(f'''
+                       {message}.
+
+                        You need to download the ChromeDriver that is compatible with your version of Chrome.
+
+                        Please refer to the information about GOOGLE CHROME & CHROME_DRIVER above. 
+
+                        Once you are done downloading the ChromeDriver, you unzip it and then 
+
+                        replace the current one by placing the new one in the same folder as
+
+                        this "The Tube Scraper.exe".
+
+                    ''')
+            input('\nPress Enter To Exit.\n')
+            sys.exit(1)
 
         elif 'INTERNET' in str(exp):
 
@@ -56,28 +75,17 @@ def tuber_droid(search_name: str) -> None:
                             ''')
 
             input('\nPress Enter To Exit.\n')
+            sys.exit(1)
 
         elif WebDriverException:
 
             if 'version' in str(exp):
 
-                print('\nPlease make sure your version of Google Chrome is at least version 97.\n'
+                print('\nPlease make sure your version of Google Chrome is at least version 103.\n'
 
                       'Open your Chrome browser and go to "Menu -> Help -> About Google Chrome"\n'
 
-                      'to update your web browser.\n'
-
-                      'If you get this error message after updating your Google Chrome, then you\n'
-
-                      'will need to download an updated version of chromedriver.\n'
-
-                      'Visit https://chromedriver.chromium.org/downloads and download the chromedriver\n'
-
-                      'that matches your version of Google Chrome. Once downloaded, unzip the download\n'
-
-                      'and copy the file named chromedriver.exe & paste it in the same place as\n'
-
-                      'this tube_channel_downloader.exe. Then you will be good to go!')
+                      'to update your web browser.\n')
 
             else:
                 print('\nSomething went wrong, please make sure you do not disturb the Google Chrome window\n'
@@ -85,10 +93,42 @@ def tuber_droid(search_name: str) -> None:
                       'while it works when you try again.')
 
             input('\nPress Enter To Exit & try again.\n')
+            sys.exit(1)
+
+        elif 'Timed out receiving message from renderer' or 'cannot determine loading status' in str(exp):
+            print('\nGoogle Chrome is taking too long to respond :( .')
+
+        elif 'ERR_NAME_NOT_RESOLVED' or 'ERR_CONNECTION_CLOSED' or 'unexpected command response' in str(exp):
+            print('\nYour internet connection may have been interrupted.')
+            print('Please make sure you\'re still connected to the internet and try again.')
+
+        else:
+            print('\nSomething went wrong, please make sure you do not disturb the Google Chrome window\n'
+                  'while it works when you try again.')
+
+        input('\nPress Enter to Exit & Try Again.')
+        sys.exit(1)
+
+
+def detect_browser_version(filename):
+    parser = Dispatch("Scripting.FileSystemObject")
+    try:
+        version = parser.GetFileVersion(filename)
+    except Exception:
+        return None
+    return version
+
+
+if __name__ == "__main__":
+    script_summary()
+    absolute_paths = [r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+                      r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
+    users_browser_version = list(filter(None, [detect_browser_version(p) for p in absolute_paths]))[0]
+    print('YOUR GOOGLE CHROME VERSION: ' + users_browser_version)
+    print(f'IF YOU NEED TO DOWNLOAD THE CHROME_DRIVER: https://chromedriver.chromium.org/downloads\n')
 
 
 def main() -> None:
-    script_summary()
     search_name = input('\nEnter Artist / Channel Name: ').title().strip()
     if len(search_name) >= 1:
         tuber_droid(search_name)
